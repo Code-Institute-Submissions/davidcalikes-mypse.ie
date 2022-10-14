b8d3ffafe2a0d148af09dfb9fac77f832372f2f0
davidcalikes <davidcalikes@gmail.com>
2022-10-14 20:34:32 +0000

    feat: Add Enrolled Pupil Model

--- passport/models.py
+++ passport/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from user.models import CustomUser
 
-# Create your models here.
+
+class EnrolledPupil(models.Model):
+    pupil_last_name = models.CharField(
+        max_length=200, default='')
+    pupil_first_name = models.CharField(
+        max_length=200, default='')
+    school_name = models.CharField(
+        max_length=200, default='')
+    teacher_name = models.CharField(
+        max_length=200, default='')
+    school_roll_no = models.CharField(
+        max_length=200, default='')
+    pupil_id = models.PositiveIntegerField(
+        default=0, unique=True)
+    school_email = models.EmailField(max_length=200)
+    created_by = models.ForeignKey(CustomUser,
+                                   on_delete=models.CASCADE,
+                                   related_name='created_by')
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __int__(self):
+        return self.pupil_id

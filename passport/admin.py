ad4340333842917fe71ee09859bef10182faf01e
davidcalikes <davidcalikes@gmail.com>
2022-10-14 20:50:27 +0000

    chore: Register EnrolledPupil model in admin

--- passport/admin.py
+++ passport/admin.py
@@ -1,1 +1,17 @@
+from django.contrib import admin
+from .models import EnrolledPupil
 
+
+@admin.register(EnrolledPupil)
+class EnrolledPupilAdmin(admin.ModelAdmin):
+    list_display = ('pupil_first_name', 'pupil_last_name',
+                    'school_name', 'teacher_name', 'school_roll_no',
+                    'pupil_id', 'school_email', 'created_by',
+                    'last_updated')
+    search_fields = ('pupil_first_name', 'pupil_last_name',
+                     'school_name', 'teacher_name', 'school_roll_no',
+                     'pupil_id', 'school_email', 'created_by',
+                     'last_updated')
+
+    def approved_entry(self, request, queryset):
+        queryset.update(approved=True)

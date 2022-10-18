8c9a360e913871f2bde8828d1050cafb8dda9bee
davidcalikes <davidcalikes@gmail.com>
2022-10-18 11:37:25 +0000

    chore: Add summernote urls to patterns in mypse settings

--- mypse/urls.py
+++ mypse/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('passport.urls'), name='passport'),
     path('accounts/', include('allauth.urls')),
+    path('summernote/', include('django_summernote.urls'))
 ]

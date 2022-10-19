60cc84f281be4ac5554aa229d60046aa34056713
davidcalikes <davidcalikes@gmail.com>
2022-10-19 10:43:55 +0000

    feat: Add url for validate pupil id

--- passport/urls.py
+++ passport/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('<int:pupil_id>/', views.EnrolledPupilRecord.as_view(), name='enrolled_pupil_record'),
     path('update/<int:pk>', views.UpdatePupilRecord.as_view(), name='update_pupil_record'),
     path('delete/<int:pk>', views.DeletePupilRecord.as_view(), name='delete_pupil_record'),
+    path('validate_pupil_id/', views.ValidatePupilId.as_view(), name='validate_pupil_id'),
     path('add_passport/', views.AddPassport.as_view(), name='add_passport'),
     path('passport_list/', views.PassportList.as_view(), name='passport_list'),
     path('passport_detail/<slug:slug>/', views.PassportDetail.as_view(), name='passport_detail'),

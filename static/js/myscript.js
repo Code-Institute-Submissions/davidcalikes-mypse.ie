87a8a8c41b6e50cb06685f46b68084981135f9d3
davidcalikes <davidcalikes@gmail.com>
2022-10-29 14:38:08 +0000

    fix: Add else condition to validateForm in myscript.js to allow form submission

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -2,3 +2,15 @@ function setId() {
     let pupilId= document.getElementById("valid-pupil-id").innerHTML
     localStorage.setItem("id", pupilId);
 }
+
+function validateForm() {
+    let pupilId = document.forms["form"]["pupil_id"].value;
+    console.log(document.forms["form"]["pupil_id"].value)
+    let storedId = localStorage.getItem("id");
+        if (pupilId !=  storedId) {
+        alert("Incorrect ID");
+        return false;
+        } else {
+        return true;
+  }
+}

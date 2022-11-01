72f27eb11677b5ea908afe9ef44e31bb834dffb3
davidcalikes <davidcalikes@gmail.com>
2022-11-01 12:22:07 +0000

    feat: Add JavaScript Function to prevent unauthorised teachers from viewing passports

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -20,6 +20,28 @@ function setEditPupilId() {
     localStorage.setItem("id", pupilId);
 }
 
+function setTeacherId() {
+    let teacherId= document.getElementById("set-teacher-id").innerHTML
+    console.log(teacherId)
+    localStorage.setItem("id", teacherId);
+}
+
+function validateTeacher() {
+    let teacherId= document.getElementById("passport-teacher-id").innerHTML
+    console.log(teacherId)
+    let storedId = localStorage.getItem("id");
+    console.log(storedId)
+        if (teacherId !==  storedId) {
+        alert("Incorrect Teacher ID!");
+        window.history.back();
+        return false;
+        } else {
+        document.getElementById("passport-blur").style.filter = "blur(0px)";
+        alert("Teacher Id Is Valid!")
+        return true;
+  }
+}
+
 function validateForm() {
     let pupilId = document.forms["form"]["pupil_id"].value;
     console.log(document.forms["form"]["pupil_id"].value)
@@ -30,5 +52,5 @@ function validateForm() {
         return false;
         } else {
         return true;
-  }
+    }
 }

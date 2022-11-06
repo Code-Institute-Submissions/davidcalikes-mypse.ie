eaa42df51472f16b3b1d985a0b5f0072eebfd536
davidcalikes <davidcalikes@gmail.com>
2022-11-06 09:39:59 +0000

    feat: Add image and adjust layout of heading content in pupil list template

--- mypse/settings.py
+++ mypse/settings.py
@@ -60,7 +60,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = 'login_success'
 LOGOUT_REDIRECT_URL = '/'
 
-# CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 SUMMERNOTE_CONFIG = {
     'summernote': {
